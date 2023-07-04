040c3d17851e1193bbe9216511c5cb5fc35adcfb
Rafael <3062395230311@ingenieria.usac.edu.gt>
2023-07-03 20:46:20 -0600

    Modificación: Subiendo los primeros ejemplos en Java de Alura

---
+++ Java/Ejercico1/src/main/java/com/mycompany/ejercico1/Ejercico1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercico1;
+
+/**
+ *
+ * @author mende
+ */
+public class Ejercico1 {
+
+    public static void main(String[] args) {
+        System.out.println("HOLA MUNDO, XD");
+    }
+}
---
+++ Java/hello_world/src/hello_world/Ejemplo_UNO.java
@@ -0,0 +1,10 @@
+package hello_world;
+
+public class Ejemplo_UNO {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World ...(*￣０￣)ノ");
+	}
+
+}
